fc729714abdbe6c690aa740f079509810b3dbe25
Clement Deville <cdeville@made-f0Br5s5.clusters.42paris.fr>
2024-12-05 17:18:13 +0100

    remove .vscode ?

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 15:04:35 by cdeville          #+#    #+#             */
-/*   Updated: 2024/10/11 19:31:26 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/11/19 16:56:06 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,19 @@
 
 int main( void )
 {
-	Fixed a;
+	Fixed a(-8388608);
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	std::cout << a << std::endl;
+	int test = -2147483647;
+	test = test << 8 >> 8;
+
+	int x = -1;
+	float y = x;
+
+	std::cout << "new x: " << x << std::endl;
+	std::cout << "new y: " << y << std::endl;
+
+
+	std::cout << "test:" << test << "\na:" << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
