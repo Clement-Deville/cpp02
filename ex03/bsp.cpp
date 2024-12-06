075c0b634ff2d56c1fc144b97fd7d83e209b1185
Clement Deville <cdeville@made-f0Br5s4.clusters.42paris.fr>
2024-12-06 19:55:17 +0100

    Branch

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:55:40 by cdeville          #+#    #+#             */
-/*   Updated: 2024/12/06 14:53:43 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/12/06 18:43:17 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ Fixed	vector_size(const Point &A, const Point &B)
 
 	std::cout << "A: " << A << std::endl;
 	std::cout << "B: " << B << std::endl;
-	// ab_size = ((B.getX() - A.getX()) * (B.getX() - A.getX())
-	// 	+ (B.getY() - A.getY()) * (B.getY() - A.getY()));
-	ab_size = (B.getX() - A.getX());
+	ab_size = ((B.getX() - A.getX()) * (B.getX() - A.getX())
+		+ (B.getY() - A.getY()) * (B.getY() - A.getY()));
+	// ab_size = (B.getX() - A.getX());
 	std::cout << "ab_size " << ab_size << std::endl;
 	std::cout << "TMP: " << ab_size.toFloat() << std::endl;
 	ab_size = Fixed((float)std::sqrt((double)ab_size.toFloat()));
@@ -44,8 +44,8 @@ bool	check_degenerate_triangle(Point const a, Point const b, Point const c)
 	std::cout << "Side sizes:\nAB: " << ab_size << "\nBC: "
 		<< bc_size << "\nAC: " << ac_size << std::endl;
 	if ((vector_size(a, b) + vector_size(b, c) <= vector_size(a, c))
-		&& (vector_size(a, c) + vector_size(b, c) <= vector_size(a, b))
-		&& (vector_size(a, b) + vector_size(a, c) <= vector_size(b, c)))
+		|| (vector_size(a, c) + vector_size(b, c) <= vector_size(a, b))
+		|| (vector_size(a, b) + vector_size(a, c) <= vector_size(b, c)))
 		return (true);
 	return (false);
 }
@@ -54,38 +54,70 @@ bool	bsp( Point const a, Point const b, Point const c, Point const point)
 {
 	Fixed w1;
 	Fixed w2;
+	Fixed Ax(a.getX()), Ay(a.getY()), Bx(b.getX()), By(b.getY()),
+		Cx(c.getX()), Cy(c.getY()), Px(point.getX()), Py(point.getY());
 
 	if (check_degenerate_triangle(a, b, c) == true)
 	{
 		std::cerr << "The triangle is a degenerate triangle, the point can't be inside" << std::endl;
 		return (false);
 	}
-	if (b.getX() - a.getX() != Fixed(0)
-		&& (c.getX() - a.getX()) * (b.getY() - a.getY())
-			- (c.getY() - a.getY()) * (b.getX() - a.getX()) != Fixed(0))
+	// if (b.getX() - a.getX() != Fixed(0)
+	// 	&& (c.getX() - a.getX()) * (b.getY() - a.getY())
+	// 		- (c.getY() - a.getY()) * (b.getX() - a.getX()) != Fixed(0))
+	// {
+	// 	w2 = a.getY() * (b.getX() - a.getX())
+	// 		+ (point.getX() - a.getX()) * (b.getY() - a.getY())
+	// 		- point.getY() * (b.getX() - a.getX());
+	// 	w2 /= (c.getX() - a.getX()) * (b.getY() - a.getY())
+	// 		- (c.getY() - a.getY()) * (b.getX() - a.getX());
+	// 	w1 = point.getX() - a.getX() - w2 * (c.getX() - a.getX());
+	// 	w1 /= b.getX() - a.getX();
+	// 	// w2 = a.getY() * (b.getX() - a.getX())
+	// 	// 	+ (point.getX() - a.getX()) * (b.getY() - a.getY())
+	// 	// 	- point.getY() * (b.getX() - a.getX());
+	// 	// w2 /= (c.getX() - a.getX()) * (b.getY() - a.getY())
+	// 	// 	- (c.getY() - a.getY()) * (b.getX() - a.getX());
+	// 	// w1 = point.getX() - a.getX() - w2 * (c.getX() - a.getX());
+	// 	// w1 /= b.getX() - a.getX();
+	// 	std::cout << "\n1--->" << std::endl;
+	// }
+	// else
+	// {
+	// 	w1 = a.getX() * (c.getY() - a.getY())
+	// 	+ (point.getY() - a.getY()) * (c.getX() - a.getX())
+	// 	- point.getX() * (c.getY() - a.getY());
+	// 	w1 /= (b.getY() - a.getY()) * (c.getX() - a.getX())
+	// 		- (b.getX() - a.getX()) * (c.getY() - a.getY());
+	// 	w2 = point.getY() - a.getY()
+	// 		- w1 * (b.getY() - a.getY());
+	// 	w2 /= c.getY() - a.getY();
+	// 	// 		w1 = a.getX() * (c.getY() - a.getY())
+	// 	// + (point.getY() - a.getY()) * (c.getX() - a.getX())
+	// 	// - point.getX() * (c.getY() - a.getY());
+	// 	// w1 /= (b.getY() - a.getY()) * (c.getX() - a.getX())
+	// 	// 	- (b.getX() - a.getX()) * (c.getY() - a.getY());
+	// 	// w2 = point.getY() - a.getY()
+	// 	// 	- w1 * (b.getY() - a.getY());
+	// 	// w2 /= c.getY() - a.getY();
+	// 	std::cout << "\n2--->" << std::endl;
+	// }
+	if (Cy - Ay != Fixed(0))
 	{
-		w2 = a.getY() * (b.getX() - a.getX())
-			+ (point.getX() - a.getX()) * (b.getY() - a.getY())
-			- point.getY() * (b.getX() - a.getX());
-		w2 /= (c.getX() - a.getX()) * (b.getY() - a.getY())
-			- (c.getY() - a.getY()) * (b.getX() - a.getX());
-		w1 = point.getX() - a.getX() - w2 * (c.getX() - a.getX());
-		w1 /= b.getX() - a.getX();
+		w1 = Ax * (Cy - Ay) + (Py - Ay) * (Cx - Ax) - Px * (Cy - Ay);
+		w1 /= (By - Ay) * (Cx - Ax) - (Bx - Ax) * (Cy - Ay);
+
+		w2 = Py - Ay - w1 * (By - Ay);
+		w2 /= Cy - Ay;
 	}
 	else
 	{
-		w1 = a.getX() * (c.getY() - a.getY())
-		+ (point.getY() - a.getY()) * (c.getX() - a.getX())
-		- point.getX() * (c.getY() - a.getY());
-		w1 /= (b.getY() - a.getY()) * (c.getX() - a.getX())
-			- (b.getX() - a.getX()) * (c.getY() - a.getY());
-		w2 = point.getY() - a.getY()
-			- w1 * (b.getY() - a.getY());
-		w2 /= c.getY() - a.getY();
+		
 	}
-	if (w1 == Fixed(0) && w1 < Fixed(1)
-		&& w2 == Fixed(0) && w2 < Fixed(1)
-		&& w1 + w2 < Fixed(1))
+	std::cout << "\nSTD::\nW1: " << w1 << "\nW2: " << w2 << std::endl;
+	if (w1 >= Fixed(0)
+		&& w2 >= Fixed(0)
+		&& w1 + w2 <= Fixed(1))
 		return (true);
 	return (false);
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 15:04:35 by cdeville          #+#    #+#             */
-/*   Updated: 2024/12/05 18:28:33 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/12/06 15:57:09 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,21 @@ int	main( void )
 		std::cout << "The point " << Point(2,3) << " is in the Triangle"
 		<< Point(10.0f, -5.0f) << " " << Point(3.5f, 6.0f)
 		<< " " << Point(-1.0f, -5.0f)
+			<< std::endl;
+	else
+	{
+		std::cout << "The point " << Point(2,3) << " is not in the Triangle"
+		<< Point(10.0f, -5.0f) << " " << Point(3.5f, 6.0f)
+		<< " " << Point(-1.0f, -5.0f)
+			<< std::endl;
+	}
+	if (bsp(Point(1.0f, 2.0f),
+			Point(1.f, -1.0f),
+			Point(-1.0f, -2.0f),
+			Point(3.0f, 3.0f)))
+		std::cout << "The point " << Point(3,3) << " is in the Triangle"
+		<< Point(1.0f, 2.0f) << " " << Point(1.0f, -1.0f)
+		<< " " << Point(-1.0f, -2.0f)
 			<< std::endl;
 	else
 	{
