48e866a1f0079af69ab4b60f2882aafc0cb18da1
cdeville <cdeville@student.42.fr>
2024-12-09 20:40:43 +0100

    Suite new BSP, need to check main examples when on edge

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,129 +6,90 @@
 /*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:55:40 by cdeville          #+#    #+#             */
-/*   Updated: 2024/12/07 16:54:56 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/12/09 20:37:15 by cdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-Fixed	vector_size(const Point &A, const Point &B)
-{
-	Fixed ab_size;
-	// float tmp;
+// Fixed	vector_size(const Point &A, const Point &B)
+// {
+// 	Fixed ab_size;
+// 	// float tmp;
 
-	std::cout << "A: " << A << std::endl;
-	std::cout << "B: " << B << std::endl;
-	ab_size = ((B.getX() - A.getX()) * (B.getX() - A.getX())
-		+ (B.getY() - A.getY()) * (B.getY() - A.getY()));
-	// ab_size = (B.getX() - A.getX());
-	std::cout << "ab_size " << ab_size << std::endl;
-	std::cout << "TMP: " << ab_size.toFloat() << std::endl;
-	ab_size = Fixed((float)std::sqrt((double)ab_size.toFloat()));
-	return (ab_size);
-}
+// 	std::cout << "A: " << A << std::endl;
+// 	std::cout << "B: " << B << std::endl;
+// 	ab_size = ((B.getX() - A.getX()) * (B.getX() - A.getX())
+// 		+ (B.getY() - A.getY()) * (B.getY() - A.getY()));
+// 	// ab_size = (B.getX() - A.getX());
+// 	std::cout << "ab_size " << ab_size << std::endl;
+// 	std::cout << "TMP: " << ab_size.toFloat() << std::endl;
+// 	ab_size = Fixed((float)std::sqrt((double)ab_size.toFloat()));
+// 	return (ab_size);
+// }
+
+
+// bool	check_degenerate_triangle(Point const a, Point const b, Point const c)
+// {
+// 	// //! NOT FIXED ?
+// 	// if (((c.getY() - a.getY()) * (b.getX() - a.getX())
+// 	// - (b.getY() - a.getY()) * (c.getX() - a.getX()))
+// 	// / (c.getX() - a.getX())
+// 	// == Fixed(0))
+// 	// 	return (true);
+// 	// return (false);
+// 	Fixed ab_size = vector_size(a, b);
+// 	Fixed bc_size = vector_size(c, b);
+// 	Fixed ac_size = vector_size(a, c);
+
+// 	std::cout << "Side sizes:\nAB: " << ab_size << "\nBC: "
+// 		<< bc_size << "\nAC: " << ac_size << std::endl;
+// 	if ((vector_size(a, b) + vector_size(b, c) <= vector_size(a, c))
+// 		|| (vector_size(a, c) + vector_size(b, c) <= vector_size(a, b))
+// 		|| (vector_size(a, b) + vector_size(a, c) <= vector_size(b, c)))
+// 		return (true);
+// 	return (false);
+// }
 
 Fixed	get_area(const Point &A, const Point &B, const Point &C)
 {
-	Fixed	Base, Height;
-
-	Base = vector_size(A, B);
-	Height = 
-}
-
-bool	check_degenerate_triangle(Point const a, Point const b, Point const c)
-{
-	// //! NOT FIXED ?
-	// if (((c.getY() - a.getY()) * (b.getX() - a.getX())
-	// - (b.getY() - a.getY()) * (c.getX() - a.getX()))
-	// / (c.getX() - a.getX())
-	// == Fixed(0))
-	// 	return (true);
-	// return (false);
-	Fixed ab_size = vector_size(a, b);
-	Fixed bc_size = vector_size(c, b);
-	Fixed ac_size = vector_size(a, c);
-
-	std::cout << "Side sizes:\nAB: " << ab_size << "\nBC: "
-		<< bc_size << "\nAC: " << ac_size << std::endl;
-	if ((vector_size(a, b) + vector_size(b, c) <= vector_size(a, c))
-		|| (vector_size(a, c) + vector_size(b, c) <= vector_size(a, b))
-		|| (vector_size(a, b) + vector_size(a, c) <= vector_size(b, c)))
-		return (true);
-	return (false);
+	Fixed area = Fixed(0.5f) * ( A.getX() * (B.getY() - C.getY()) + B.getX() * (C.getY() - A.getY()) + C.getX() * (A.getY() - B.getY()) ) ;
+	if (area < Fixed(0))
+		return (area * Fixed(-1));
+	return (area);
 }
 
 bool	bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed w1;
-	Fixed w2;
-	Fixed Ax(a.getX()), Ay(a.getY()), Bx(b.getX()), By(b.getY()),
-		Cx(c.getX()), Cy(c.getY()), Px(point.getX()), Py(point.getY());
-
-	if (check_degenerate_triangle(a, b, c) == true)
-	{
-		std::cerr << "The triangle is a degenerate triangle, the point can't be inside" << std::endl;
-		return (false);
-	}
-	// if (b.getX() - a.getX() != Fixed(0)
-	// 	&& (c.getX() - a.getX()) * (b.getY() - a.getY())
-	// 		- (c.getY() - a.getY()) * (b.getX() - a.getX()) != Fixed(0))
+	// Fixed w1;
+	// Fixed w2;
+	// Fixed Ax(a.getX()), Ay(a.getY()), Bx(b.getX()), By(b.getY()),
+	// 	Cx(c.getX()), Cy(c.getY()), Px(point.getX()), Py(point.getY());
+	Fixed other_areas[3];
+	
+	// if (check_degenerate_triangle(a, b, c) == true)
 	// {
-	// 	w2 = a.getY() * (b.getX() - a.getX())
-	// 		+ (point.getX() - a.getX()) * (b.getY() - a.getY())
-	// 		- point.getY() * (b.getX() - a.getX());
-	// 	w2 /= (c.getX() - a.getX()) * (b.getY() - a.getY())
-	// 		- (c.getY() - a.getY()) * (b.getX() - a.getX());
-	// 	w1 = point.getX() - a.getX() - w2 * (c.getX() - a.getX());
-	// 	w1 /= b.getX() - a.getX();
-	// 	// w2 = a.getY() * (b.getX() - a.getX())
-	// 	// 	+ (point.getX() - a.getX()) * (b.getY() - a.getY())
-	// 	// 	- point.getY() * (b.getX() - a.getX());
-	// 	// w2 /= (c.getX() - a.getX()) * (b.getY() - a.getY())
-	// 	// 	- (c.getY() - a.getY()) * (b.getX() - a.getX());
-	// 	// w1 = point.getX() - a.getX() - w2 * (c.getX() - a.getX());
-	// 	// w1 /= b.getX() - a.getX();
-	// 	std::cout << "\n1--->" << std::endl;
+	// 	return (false);
 	// }
-	// else
-	// {
-	// 	w1 = a.getX() * (c.getY() - a.getY())
-	// 	+ (point.getY() - a.getY()) * (c.getX() - a.getX())
-	// 	- point.getX() * (c.getY() - a.getY());
-	// 	w1 /= (b.getY() - a.getY()) * (c.getX() - a.getX())
-	// 		- (b.getX() - a.getX()) * (c.getY() - a.getY());
-	// 	w2 = point.getY() - a.getY()
-	// 		- w1 * (b.getY() - a.getY());
-	// 	w2 /= c.getY() - a.getY();
-	// 	// 		w1 = a.getX() * (c.getY() - a.getY())
-	// 	// + (point.getY() - a.getY()) * (c.getX() - a.getX())
-	// 	// - point.getX() * (c.getY() - a.getY());
-	// 	// w1 /= (b.getY() - a.getY()) * (c.getX() - a.getX())
-	// 	// 	- (b.getX() - a.getX()) * (c.getY() - a.getY());
-	// 	// w2 = point.getY() - a.getY()
-	// 	// 	- w1 * (b.getY() - a.getY());
-	// 	// w2 /= c.getY() - a.getY();
-	// 	std::cout << "\n2--->" << std::endl;
-	// }
-	if (Cy - Ay != Fixed(0))
+	Fixed Triangle_area = get_area(a, b , c);
+	std::cout << "Triangle area: " << Triangle_area << std::endl;
+	if (Triangle_area == Fixed(0))
 	{
-		w1 = Ax * (Cy - Ay) + (Py - Ay) * (Cx - Ax) - Px * (Cy - Ay);
-		w1 /= (By - Ay) * (Cx - Ax) - (Bx - Ax) * (Cy - Ay);
-
-		w2 = Py - Ay - w1 * (By - Ay);
-		w2 /= Cy - Ay;
+ 		std::cerr << "The triangle is a degenerate triangle, the point can't be inside" << std::endl;
+		return (false); //! Checking If degenerate
 	}
-	else
-	{
-		w2 =
+	other_areas[0] = get_area(a, b , point);
+	other_areas[1] = get_area(a, point , c);
+	other_areas[2] = get_area(point, b , c);
 
-		w1 = Py - Ay - w2 * (Cy - Ay);
-		w1 /= By - Ay;
-	}
-	std::cout << "\nSTD::\nW1: " << w1 << "\nW2: " << w2 << std::endl;
-	if (w1 >= Fixed(0)
-		&& w2 >= Fixed(0)
-		&& w1 + w2 <= Fixed(1))
+	std::cout << "\n\nTriangle Area: " << Triangle_area << "\n";
+	std::cout << "Area 1: " << other_areas[0] << "\n";
+	std::cout << "Area 2: " << other_areas[1] << "\n";
+	std::cout << "Area 3: " << other_areas[2] << "\n\n";
+	if (other_areas[0] + other_areas[1] + other_areas[2] <= Triangle_area
+		&& other_areas[0] != 0 && other_areas[1] != 0 && other_areas[2] != 0)
 		return (true);
-	return (false);
+	else
+		return (false);
+
 }
